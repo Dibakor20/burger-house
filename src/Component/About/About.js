f88a74d63a98c67ba4c9bdfbf40ba1854647aad1
Debakor10 <debakor10@gmail.com>
2021-12-28 22:48:44 +0600

    update

--- src/Component/About/About.js
+++ src/Component/About/About.js
@@ -9,7 +9,7 @@ const About = () => {
             <div class="row d-flex align-items-center">
                 <div class="col-md-6">
                     <h2 className=''>Welcome to Burger House!</h2>
-                    <p className="paragraph">“Would you like millions of new customers to enjoy your amazing food and groceries? So would we!It's simple: we list your menu and product lists online, help you process orders, pick them up, and deliver them to hungry pandas – in a heartbeat.”</p>
+                    <p className="paragraph">“Would you like millions of new customers to enjoy your amazing food and groceries? So would we!It's simple we list your menu and product lists online, help you process orders, pick them up, and deliver them to hungry pandas – in a heartbeat.”</p>
                     <span>- Debakor Acharjee -</span>
                 </div>
                 <div class="col-md-6 text-center">
